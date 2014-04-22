bff8bd9def1f166acdee5d9638a9df0e142699e0
dwoowb <dwoo92@gmail.com>
2014-04-22 17:44:54 -0400

    Starts Follow model

---
+++ app/models/follow.rb
@@ -0,0 +1,18 @@
+class Follow < ActiveRecord::Base
+
+  belongs_to(
+    :follower,
+    class_name: "User",
+    foreign_key: :follower_id,
+    primary_key: :id
+  )
+
+  belongs_to(
+    :followee,
+    class_name: "User",
+    foreign_key: :followee_id,
+    primary_key: :id
+  )
+
+  validates :followee, :follower, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140422184929) do
+ActiveRecord::Schema.define(version: 20140422213553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,16 @@ ActiveRecord::Schema.define(version: 20140422184929) do
   end
 
   add_index "comments", ["commenter_id"], name: "index_comments_on_commenter_id", using: :btree
+
+  create_table "follows", force: true do |t|
+    t.integer  "follower_id", null: false
+    t.integer  "followee_id", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "follows", ["followee_id"], name: "index_follows_on_followee_id", using: :btree
+  add_index "follows", ["follower_id"], name: "index_follows_on_follower_id", using: :btree
 
   create_table "reblogs", force: true do |t|
     t.integer  "reblogger_id", null: false
