0ec1026b70cbfab09bec58a212a49934d70db110
App Academy Student <student@appacademy.io>
2014-04-25 16:40:10 -0400

    Adds seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,18 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+dwoowb = User.create!({username: 'dwoowb', email: 'dwoowb@gmail.com', password: 'foobar'})
+gavin = User.create!({username: 'gavin', email: 'gavin@gmail.com', password: 'foobar'})
+
+dwoowb.tracks.create!({title: 'Paradise Awaits', artist: 'ZHU'})
+dwoowb.tracks.create!({title: 'Love Strong', artist: 'Moon Boots'})
+dwoowb.tracks.create!({title: 'Feel it in my Bones', artist: 'Tiesto'})
+dwoowb.tracks.create!({title: 'Promise Me', artist: 'Motez'})
+
+
+gavin.tracks.create!({title: 'Your Heart will Lead You Home', artist: 'Kenny Loggins'})
+gavin.tracks.create!({title: 'La Gasolina', artist: 'Pitbull'})
+gavin.tracks.create!({title: 'Hey Brother', artist: 'Avicii'})
+gavin.tracks.create!({title: 'A Bumpy Ride', artist: 'Mohombi'})
+
+
